be3f7b3a0a1a546878bce615b57cfaf2f7016d92
BrainBalaka <xainsz.balaka@gmail.com>
2018-09-25 09:14:45 +0700

    add comments & change to netstandard2.0

--- src/Shared/Polyrific.Catapult.Shared.SmtpEmailNotification/SmtpSetting.cs
+++ src/Shared/Polyrific.Catapult.Shared.SmtpEmailNotification/SmtpSetting.cs
@@ -4,10 +4,29 @@ namespace Polyrific.Catapult.Shared.SmtpEmailNotification
 {
     public class SmtpSetting
     {
+        /// <summary>
+        /// The smptp server url
+        /// </summary>
         public string Server { get; set; }
+
+        /// <summary>
+        /// The smtp port
+        /// </summary>
         public int Port { get; set; }
+
+        /// <summary>
+        /// Username to authenticate to the smtp server
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Password to authenticate to the smtp server
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// The sender email address used to send the email
+        /// </summary>
         public string SenderEmail { get; set; }
     }
 }
