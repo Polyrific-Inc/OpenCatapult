485cbf8a820781193c24420f2fa64d793d35b9d1
BrainBalaka <xainsz.balaka@gmail.com>
2018-09-28 16:14:56 +0700

    add additional config for build and database provider

--- src/API/Polyrific.Catapult.Api.Data/Migrations/20180928085943_AddBuiltInPluginSeed.Designer.cs
+++ src/API/Polyrific.Catapult.Api.Data/Migrations/20180928085943_AddBuiltInPluginSeed.Designer.cs
@@ -10,7 +10,7 @@ using Polyrific.Catapult.Api.Data;
 namespace Polyrific.Catapult.Api.Data.Migrations
 {
     [DbContext(typeof(CatapultDbContext))]
-    [Migration("20180928074108_AddBuiltInPluginSeed")]
+    [Migration("20180928085943_AddBuiltInPluginSeed")]
     partial class AddBuiltInPluginSeed
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -312,10 +312,15 @@ namespace Polyrific.Catapult.Api.Data.Migrations
 
                     b.HasData(
                         new { Id = 1, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382258", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = true, Label = "Connection String", Name = "ConnectionString", PluginId = 1, Type = "string" },
-                        new { Id = 2, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382259", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "Subscription Id", Name = "SubscriptionId", PluginId = 6, Type = "string" },
-                        new { Id = 3, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225a", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "Resource Group", Name = "ResourceGroupName", PluginId = 6, Type = "string" },
-                        new { Id = 4, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225b", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "App Service", Name = "AppServiceName", PluginId = 6, Type = "string" },
-                        new { Id = 5, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225c", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Deployment Slot", Name = "DeploymentSlot", PluginId = 6, Type = "string" }
+                        new { Id = 2, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382259", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Csproj Location", Name = "CsprojLocation", PluginId = 3, Type = "string" },
+                        new { Id = 3, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225a", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Configuration", Name = "Configuration", PluginId = 3, Type = "string" },
+                        new { Id = 4, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225b", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Startup Project Name", Name = "StartupProjectName", PluginId = 5, Type = "string" },
+                        new { Id = 5, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225c", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Database Project Name", Name = "DatabaseProjectName", PluginId = 5, Type = "string" },
+                        new { Id = 6, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225d", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Configuration", Name = "Configuration", PluginId = 5, Type = "string" },
+                        new { Id = 7, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225e", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "Subscription Id", Name = "SubscriptionId", PluginId = 6, Type = "string" },
+                        new { Id = 8, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225f", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "Resource Group", Name = "ResourceGroupName", PluginId = 6, Type = "string" },
+                        new { Id = 9, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382260", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = true, IsSecret = false, Label = "App Service", Name = "AppServiceName", PluginId = 6, Type = "string" },
+                        new { Id = 10, ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382261", Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), IsRequired = false, IsSecret = false, Label = "Deployment Slot", Name = "DeploymentSlot", PluginId = 6, Type = "string" }
                     );
                 });
 
