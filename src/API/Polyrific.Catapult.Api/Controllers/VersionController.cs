a4e62b3211f2ad8d23654c6adce1815bc4bec8f1
Frandi Dwi <dev@rumahfrandi.com>
2018-10-17 19:01:38 +0700

    Add endpoint to check version

---
+++ src/API/Polyrific.Catapult.Api/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Polyrific, Inc 2018. All rights reserved.
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Polyrific.Catapult.Api.Controllers
+{
+    [Route("Version")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [Authorize]
+        public IActionResult Get()
+        {
+            return Ok(new {Version = PlatformServices.Default.Application.ApplicationVersion});
+        }
+    }
+}
