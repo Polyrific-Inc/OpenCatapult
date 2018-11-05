b7ee99a01f77f5d907b345645e5ba4462043639b
BrainBalaka <xainsz.balaka@gmail.com>
2018-11-05 16:27:21 +0700

    Remove ConnectionString additional config for AspNetCoreMvc

--- src/API/Polyrific.Catapult.Api.Data/EntityConfigs/PluginAdditionalConfigConfig.cs
+++ src/API/Polyrific.Catapult.Api.Data/EntityConfigs/PluginAdditionalConfigConfig.cs
@@ -13,7 +13,7 @@ namespace Polyrific.Catapult.Api.Data.EntityConfigs
             base.Configure(builder);
 
             builder.HasData(
-                new PluginAdditionalConfig { Id = 1, PluginId = 1, Name = "ConnectionString", Label = "Connection String", Type = "string", IsRequired = true, IsSecret = true, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382258" },
+                new PluginAdditionalConfig { Id = 1, PluginId = 1, Name = "AdminEmail", Label = "Admin Email", Type = "string", IsRequired = true, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382258" },
 
                 new PluginAdditionalConfig { Id = 2, PluginId = 3, Name = "CsprojLocation", Label = "Csproj Location", Type = "string", IsRequired = false, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382259" },
                 new PluginAdditionalConfig { Id = 3, PluginId = 3, Name = "Configuration", Label = "Configuration", Type = "string", IsRequired = false, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225a" },
@@ -26,8 +26,7 @@ namespace Polyrific.Catapult.Api.Data.EntityConfigs
                 new PluginAdditionalConfig { Id = 8, PluginId = 6, Name = "SubscriptionId", Label = "Subscription Id", Type = "string", IsRequired = true, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225e" },
                 new PluginAdditionalConfig { Id = 9, PluginId = 6, Name = "ResourceGroupName", Label = "Resource Group", Type = "string", IsRequired = true, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f3040438225f" },
                 new PluginAdditionalConfig { Id = 10, PluginId = 6, Name = "AppServiceName", Label = "App Service", Type = "string", IsRequired = true, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382260" },
-                new PluginAdditionalConfig { Id = 11, PluginId = 6, Name = "DeploymentSlot", Label = "Deployment Slot", Type = "string", IsRequired = false, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382261" },
-                new PluginAdditionalConfig { Id = 12, PluginId = 1, Name = "AdminEmail", Label = "Admin Email", Type = "string", IsRequired = true, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382300" }
+                new PluginAdditionalConfig { Id = 11, PluginId = 6, Name = "DeploymentSlot", Label = "Deployment Slot", Type = "string", IsRequired = false, IsSecret = false, Created = new DateTime(2018, 9, 28, 7, 23, 37, 58, DateTimeKind.Utc), ConcurrencyStamp = "c48cafcc-b3e9-4375-a2c2-f30404382261" }
             );
         }
     }
