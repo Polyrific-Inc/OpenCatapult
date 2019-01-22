cba6d930f1fa58da6fa26bfead236f71fcb41565
BrainBalaka <xainsz.balaka@gmail.com>
2019-01-22 13:19:42 +0700

    show friendly messages when running task provider

--- src/Engine/Polyrific.Catapult.Engine.Core/PluginManager.cs
+++ src/Engine/Polyrific.Catapult.Engine.Core/PluginManager.cs
@@ -158,6 +158,8 @@ namespace Polyrific.Catapult.Engine.Core
             {
                 if (process != null)
                 {
+                    Console.WriteLine($"Invoking Task Provider {Path.GetFileNameWithoutExtension(pluginStartFile)}...");
+
                     if (!string.IsNullOrEmpty(securedPluginArgs))
                     {
                         _logger.LogDebug($"[PluginManager] Command: {fileName} {securedArguments}");
