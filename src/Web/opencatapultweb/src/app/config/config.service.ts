91adb3b53fe712564d6103a458237e7c1455084f
unknown <dev@rumahfrandi.com>
2019-03-20 12:04:39 +0700

    Move env config to config.json

---
+++ src/Web/opencatapultweb/src/app/config/config.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  private config: Config;
+
+  constructor(private http: HttpClient) { }
+
+  async loadConfig() {
+
+    console.debug("Loading configs...");
+    const data = await this.http.get<Config>('./config.json')
+      .toPromise();
+    this.config = data;
+    console.debug(`Configs has been loaded for environment "${this.config.environmentName}".`);
+
+  }
+
+  getConfig(): Config {
+    return this.config;
+  }
+}
+
+export interface Config {
+  apiUrl: string;
+  environmentName: string;
+}
