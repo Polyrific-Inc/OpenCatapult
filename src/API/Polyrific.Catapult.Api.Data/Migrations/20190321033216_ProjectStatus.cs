2196fb53fccaf4807c1f068d4252d05107f6b9da
BrainBalaka <xainsz.balaka@gmail.com>
2019-03-21 10:48:55 +0700

    Modify project status

---
+++ src/API/Polyrific.Catapult.Api.Data/Migrations/20190321033216_ProjectStatus.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Polyrific.Catapult.Api.Data.Migrations
+{
+    public partial class ProjectStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsArchived",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Projects",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsArchived",
+                table: "Projects",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- src/Engine/Polyrific.Catapult.Engine.Core/CatapultEngine.cs
+++ src/Engine/Polyrific.Catapult.Engine.Core/CatapultEngine.cs
@@ -19,6 +19,7 @@ namespace Polyrific.Catapult.Engine.Core
         private readonly IHealthService _healthService;
         private readonly IJobQueueService _jobQueueService;
         private readonly IJobDefinitionService _jobDefinitionService;
+        private readonly IProjectService _projectService;
         private readonly IJobLogWriter _jobLogWriter;
         private readonly ILogger<CatapultEngine> _logger;
 
@@ -27,6 +28,7 @@ namespace Polyrific.Catapult.Engine.Core
             IHealthService healthService,
             IJobQueueService jobQueueService, 
             IJobDefinitionService jobDefinitionService,
+            IProjectService projectService,
             IJobLogWriter jobLogWriter,
             ILogger<CatapultEngine> logger)
         {
@@ -35,6 +37,7 @@ namespace Polyrific.Catapult.Engine.Core
             _healthService = healthService;
             _jobQueueService = jobQueueService;
             _jobDefinitionService = jobDefinitionService;
+            _projectService = projectService;
             _jobLogWriter = jobLogWriter;
             _logger = logger;
         }
@@ -65,9 +68,12 @@ namespace Polyrific.Catapult.Engine.Core
                     else
                         jobQueue.Status = JobStatus.Completed;
 
-                    if (jobQueue.IsDeletion)
+                    if (jobQueue.Status == JobStatus.Completed && 
+                        jobQueue.IsDeletion && 
+                        jobQueue.ProjectStatus == ProjectStatusFilterType.Deleting)
                     {
-                        // Call the API to perform hard delete
+                        _logger.LogInformation($"Deleting project {jobQueue.ProjectId}");
+                        await _projectService.DeleteProject(jobQueue.ProjectId);
                     }
                 }
                 catch (Exception ex)
