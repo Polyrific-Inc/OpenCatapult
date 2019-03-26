3450f387b9e95c1e9d2c787b5ff4232d39c28e7f
BrainBalaka <xainsz.balaka@gmail.com>
2019-03-26 12:55:30 +0700

    Implement generic task provider

---
+++ src/Plugins/GenericTaskProvider/Polyrific.Catapult.TaskProviders.GenericCommand/src/CommandProviders/CommandTextHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Polyrific.Catapult.TaskProviders.GenericCommand.CommandProviders
+{
+    public static class CommandTextHelper
+    {
+        public static string[] SplitOnNewLine(this string input, string[] splitters = null, bool removeEmptyEntries = true)
+        {
+            if (splitters == null)
+                splitters = new string[] { "\r\n", "\r", "\n" };
+
+            return input.Split(splitters, removeEmptyEntries ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None)
+                .Select(s => s.Trim()).ToArray();
+        }
+    }
+}
