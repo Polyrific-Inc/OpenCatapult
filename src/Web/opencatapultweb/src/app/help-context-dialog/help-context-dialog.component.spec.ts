946d5ebba6f0a6afe1e819b3266024a6880468a3
BrainBalaka <xainsz.balaka@gmail.com>
2019-04-22 20:38:22 +0700

    fix unit test

--- src/Web/opencatapultweb/src/app/help-context-dialog/help-context-dialog.component.spec.ts
+++ src/Web/opencatapultweb/src/app/help-context-dialog/help-context-dialog.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelpContextDialogComponent } from './help-context-dialog.component';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 describe('HelpContextDialogComponent', () => {
   let component: HelpContextDialogComponent;
@@ -8,7 +9,22 @@ describe('HelpContextDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelpContextDialogComponent ]
+      declarations: [ HelpContextDialogComponent ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef, useValue: {
+            close: function (result) {
+
+            }
+          }
+        },
+        {
+          provide: MAT_DIALOG_DATA, useValue: []
+        }
+      ]
     })
     .compileComponents();
   }));
