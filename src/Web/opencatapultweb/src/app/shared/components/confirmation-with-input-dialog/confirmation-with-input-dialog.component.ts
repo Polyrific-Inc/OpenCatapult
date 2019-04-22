8be8266f193a3a67f93eb137b8cbe9eef3c40da2
Zulkurnain <zulkurnain.pancawardana@gmail.com>
2019-04-22 15:22:11 +0700

    revert commit on dialog component

--- src/Web/opencatapultweb/src/app/shared/components/confirmation-with-input-dialog/confirmation-with-input-dialog.component.ts
+++ src/Web/opencatapultweb/src/app/shared/components/confirmation-with-input-dialog/confirmation-with-input-dialog.component.ts
@@ -40,12 +40,4 @@ export class ConfirmationWithInputDialogComponent {
       }
     }
 
-    onKeydown(event) {
-      if(event.key === "Enter"){
-          if (this.inputControl.valid) {
-            this.dialogRef.close(true);
-          }
-      }
-    }
-
 }
