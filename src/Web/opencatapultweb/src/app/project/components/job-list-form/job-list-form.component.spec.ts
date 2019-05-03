d988ca6578f216718e9721b214f3a5bb7c418c76
BrainBalaka <xainsz.balaka@gmail.com>
2019-05-03 22:25:55 +0700

    Improve project creation workflow

---
+++ src/Web/opencatapultweb/src/app/project/components/job-list-form/job-list-form.component.spec.ts
@@ -0,0 +1,59 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JobListFormComponent } from './job-list-form.component';
+import {
+  JobConfigFormComponent,
+  TaskConfigListFormComponent,
+  TaskConfigFormComponent,
+  PullTaskConfigFormComponent,
+  GenerateTaskConfigFormComponent,
+  PushTaskConfigFormComponent,
+  MergeTaskConfigFormComponent,
+  BuildTaskConfigFormComponent,
+  DeployDbTaskConfigFormComponent,
+  DeployTaskConfigFormComponent,
+  TestTaskConfigFormComponent,
+  AdditionalConfigFieldComponent,
+  AdditionalConfigFormComponent,
+  DeleteRepositoryConfigFormComponent } from '@app/shared';
+import { MatExpansionModule, MatInputModule, MatCheckboxModule, MatSelectModule, MatStepperModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+
+describe('JobListFormComponent', () => {
+  let component: JobListFormComponent;
+  let fixture: ComponentFixture<JobListFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        JobListFormComponent,
+        JobConfigFormComponent,
+        TaskConfigListFormComponent,
+        TaskConfigFormComponent,
+        PullTaskConfigFormComponent,
+        GenerateTaskConfigFormComponent,
+        PushTaskConfigFormComponent,
+        MergeTaskConfigFormComponent,
+        BuildTaskConfigFormComponent,
+        DeployDbTaskConfigFormComponent,
+        DeployTaskConfigFormComponent,
+        TestTaskConfigFormComponent,
+        AdditionalConfigFormComponent,
+        AdditionalConfigFieldComponent,
+        DeleteRepositoryConfigFormComponent
+      ],
+      imports: [ MatExpansionModule, ReactiveFormsModule, MatInputModule, MatCheckboxModule, MatSelectModule, MatStepperModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobListFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
